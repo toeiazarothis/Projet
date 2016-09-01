689a480aa3c37ffcfc436369f15691ab4543be06
ToeiAzarothis <patrickpatrickdealmeida89000@gmail.com>
2016-09-01 14:36:37 +0200

    first

---
+++ view/v_index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Meta -->
+    <meta lang="fr">
+    <meta content="">
+    <meta name="description" content="">
+    <meta charset="utf-8">
+
+    <!-- Link -->
+    <link rel="stylesheet" href="view/css/style.css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
