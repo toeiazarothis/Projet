a4b1f6430eeaa92ba6c85adfbb35ad5b56f0968d
ToeiAzarothis <patrickpatrickdealmeida89000@gmail.com>
2016-09-14 11:29:08 +0200

    ajout connexion BDD

--- model/id.php
+++ model/id.php
@@ -1,4 +1,4 @@
 <?php
-define('UTILISATEUR','u852249137_patri');
-define('PASS','azertyuiop');
+define('UTILISATEUR','');
+define('PASS','');
  ?>
--- model/m_accueil.php
+++ model/m_accueil.php
@@ -2,7 +2,7 @@
   include('id.php');
   //chiens
   function listeMoiLesChiens(){
-    $connexion = new PDO('mysql:host=mysql.hostinger.fr;dbname=u852249137_patri', UTILISATEUR, PASS);
+    $connexion = new PDO('mysql:host=mysql.hostinger.fr;dbname=', UTILISATEUR, PASS);
     $liste = $connexion->query('SELECT nom_ch FROM chiens GROUP BY nom_ch ASC');
     $result = $liste->fetchALL();
     return $result;
