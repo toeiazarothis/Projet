b6412ff9a863397265fc218f48cb00b1f7d164a7
ToeiAzarothis <patrickpatrickdealmeida89000@gmail.com>
2016-09-20 15:39:30 +0200

    prof

--- view/v_prof.php
+++ view/v_prof.php
@@ -15,13 +15,57 @@
     <meta property="og:image" content="image a mettre" />
 
     <!-- Link -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link  href="css/cover.css" rel="stylesheet">
+    <link href="../view/css/bootstrap.css" rel="stylesheet">
+    <link  href="../view/css/connecter.css" rel="stylesheet">
 
 
-    <title>SchoolNotes</title>
+    <title>Professeur - iSchool Notes</title>
   </head>
   <body>
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">iSchool Note</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#" class="page-scroll">Accueil</a></li>
+            <!-- <li><a href="#note" class="page-scroll">Note</a></li>
+            <li><a href="#devoir" class="page-scroll">Devoir</a></li> -->
+            <li><a href="c_eleve.php">Eleve</a></li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Parent <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#absence">Absence</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Information administrative</li>
+                <li><a href="#prof">Appreciation de prof</a></li>
+                <li><a href="#administration">Appreciation de la direction</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#">Deconnexion</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <header>
+      <div class="container">
+        <div class="intro-text"><br><br><br><br><br><br><br><br>
+        </div>
+      </div>
+    </header>
 
   </body>
 </html>
