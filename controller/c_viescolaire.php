abdeb5dc81ab7fd8deac978e6fb73b398f13a914
Moody <jordan.larue.du.10@hotmail.fr>
2016-09-21 10:28:41 +0200

    up by moody

--- controller/c_viescolaire.php
+++ controller/c_viescolaire.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include ('../model/m_fonctions.php');
 if(!isset($_SESSION['users'], $_SESSION['userid']) && $_SESSION['stats'] != 'viescolaire') {
 	return header('Location: c_index.php');
 }
@@ -14,5 +15,4 @@ if($_SESSION['stats'] != 'viescolaire') {
 		return header('Location: c_parent.php');
 	}
 }
-include ('../model/m_fonctions.php')
 include ('../view/v_viescolaire.php');?>
