57ad6aea5cbe9e794224becb08188d18ac86cfbb
josselynh <josselynh@yahoo.com>
2016-10-03 22:53:13 +0200

    Update v_event.php

--- view/v_event.php
+++ view/v_event.php
@@ -38,7 +38,7 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="accueil">Deconnexion</a></li>
+            <li><a href="accueil">Déconnexion</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
