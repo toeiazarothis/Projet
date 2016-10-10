6a69a07fce123914ad0b805160553ddac95b1537
Moodys <jordan.larue.du.10@hotmail.fr>
2016-10-10 09:29:52 +0200

    ajout de la fonction modifier et supprimer eleve

--- controller/c_admin.php
+++ controller/c_admin.php
@@ -23,19 +23,5 @@ if (isset($_POST['nom_eleve'], $_POST['prenom_eleve'], $_POST['classe_eleve'], $
   return AddEleveForAdmin (strtolower($_POST['nom_eleve']), strtolower($_POST['prenom_eleve']), strtolower($_POST['classe_eleve']), strtolower($_POST['nom_parent']), strtolower($_POST['prenom_parent']), strtolower($_POST['adresse_parent']), strtolower($_POST['email_parent']), strtolower($_POST['tel_parent']));
 }
 
-// Fait C/C par PATRICK
-if (isset($_POST['matiere'], $_POST['classe'], $_POST['nom'], $_POST['prenom'], $_POST['adresse_professeur'], $_POST['email_professeur'], $_POST['tel_professeur'])) {
-  return AddProfForAdmin (strtolower($_POST['matiere']), strtolower($_POST['classe']), strtolower($_POST['nom']), strtolower($_POST['prenom']), strtolower($_POST['adresse_professeur']), strtolower($_POST['email_professeur']), strtolower($_POST['tel_professeur']));
-}
-
-if (isset($_POST['nom_eleve'], $_POST['prenom_eleve'], $_POST['classe_eleve'], $_POST['nom_parent'], $_POST['prenom_parent'], $_POST['adresse_parent'], $_POST['email_parent'], $_POST['tel_parent'])) {
-  return AddVieScolaireForAdmin (strtolower($_POST['nom_eleve']), strtolower($_POST['prenom_eleve']), strtolower($_POST['classe_eleve']), strtolower($_POST['nom_parent']), strtolower($_POST['prenom_parent']), strtolower($_POST['adresse_parent']), strtolower($_POST['email_parent']), strtolower($_POST['tel_parent']));
-}
-
-if (isset($_POST['nom'])) {
-  return AddClasseForAdmin (strtolower($_POST['nom']));
-}
-
-//
 include ('../view/v_admin.php');
 ?>
