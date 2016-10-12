add47c2bca3c16004a874a016c80629e1241db70
josselynh <josselynh@yahoo.com>
2016-10-13 00:21:52 +0200

    Update ajoute.php

--- code a akram a integrer/ajoute.php
+++ code a akram a integrer/ajoute.php
@@ -1,6 +1,6 @@
 <?php
  include 'function.php';
   ajouteleve();
-  echo '<script> alert("ajoute élève successfully");</script>';
+  echo '<script> alert("l\'élève a été ajouté avec succès");</script>';
   header("Location:menuajouteleve.php");
 ?>
